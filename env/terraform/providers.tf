c03912d7ea69ad9a6eef655ee305a7f3b071f109
karmazynowyksiaze <ppawlowski186@gmail.com>
2024-03-09 01:22:30 +0100

    change(core): change IaC from MS Azure to AWS

--- env/terraform/providers.tf
+++ env/terraform/providers.tf
@@ -1,16 +1,12 @@
 terraform {
-  required_version = ">=1.0"
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
-provider "azurerm" {
-  features {}
+
+provider "aws" {
+  region = "eu-north-1"
 }
--- env/terraform/variables.tf
+++ env/terraform/variables.tf
@@ -1,48 +1,18 @@
-variable "resource_group_location" {
+variable "instance_count" {
+  default = "3"
+}
+
+variable "instance_tags" {
+  type    = list(string)
+  default = ["AnsibleServer", "JenkinsServer", "HTTPServer"]
+}
+
+variable "ami" {
   type    = string
-  default = "West Europe"
+  default = "ami-0914547665e6a707c" #Jammy Jellyfish 22.04 LTS
 }
 
-variable "resource_group_name_prefix" {
+variable "instance_type" {
   type    = string
-  default = "rg"
-}
-
-variable "container_group_name_prefix" {
-  type    = string
-  default = "acigroup"
-}
-
-variable "container_name_prefix" {
-  type    = string
-  default = "aci"
-}
-
-variable "image" {
-  type    = string
-  default = "ppawlowski186/jenkins-docker"
-}
-
-variable "port" {
-  type    = number
-  default = 8080
-}
-
-variable "cpu_cores" {
-  type    = number
-  default = 1
-}
-
-variable "memory_in_gb" {
-  type    = number
-  default = 2
-}
-
-variable "restart_policy" {
-  type    = string
-  default = "Always"
-  validation {
-    condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
-    error_message = "The restart_policy must be one of the following: Always, Never, OnFailure."
-  }
+  default = "t3.micro"
 }
